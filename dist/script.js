5b881da609b2c7c34235b5cda2b475800a6d0bd2
Oliver H <93524576+oliv12git@users.noreply.github.com>
2026-03-14 15:42:12 +0200

    p

--- dist/script.js
+++ dist/script.js
@@ -1,40 +1,29 @@
-function openPopup(game) {
-  const popup = document.getElementById("popup");
-  const text = document.getElementById("popup-text");
+const messages = [
+  "Initializing engine...",
+  "Loading materials...",
+  "Loading shaders...",
+  "Loading models...",
+  "Loading textures...",
+  "Initializing client...",
+  "Starting site..."
+];
 
-  if (game === "portal1") {
-    text.innerHTML = `
-      <h2>Portal</h2>
-      <p>Portal is a 2007 puzzle game by Valve focused on physics-based puzzle-solving.</p>
-    `;
-  } else if (game === "portal2") {
-    text.innerHTML = `
-      <h2>Portal 2</h2>
-      <p>Portal 2 expands the original with new mechanics, story, and cooperative gameplay.</p>
-    `;
-  } else if (game === "valve") {
-    text.innerHTML = `
-	  <h1>Easter egg!</h1>
-      <h2>Valve Corporation</h2>
-      <p>Valve is a video game developer and digital distribution company founded in 1996.</p>
-      <p>They created Steam, Half-Life, Portal, and many other iconic games.</p>
-	  <img src="valve.jpg" alt="Valve logo" class="popup-logo">
-	  <video width="240" height="144" controls>
-      <source src="cpu.mp4" type="video/mp4">
-      Your browser does not support the video tag.
-      </video>
+let index = 0;
 
-    `;
+function updateConsole() {
+  const consoleText = document.getElementById("console");
+
+  if (index < messages.length) {
+    consoleText.textContent = messages[index];
+    index++;
+    setTimeout(updateConsole, 500);
   }
-
-  popup.classList.remove("hidden");
 }
 
-function closePopup() {
-  document.getElementById("popup").classList.add("hidden");
-}
+updateConsole();
 
-window.addEventListener("load", function () {
+window.addEventListener("load", () => {
+
   const loader = document.getElementById("loading-screen");
 
   setTimeout(() => {
@@ -44,5 +33,7 @@ window.addEventListener("load", function () {
     setTimeout(() => {
       loader.style.display = "none";
     }, 500);
-  }, 2500);
+
+  }, 3500);
+
 });
